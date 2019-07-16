2f7c2138b0357c49297f4e3a4feb63a27dc44bf5
philippe <philippaudier@gmail.com>
2019-07-16 19:18:30 +0200

    Button nav bar + calendar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalendrierComponent } from './calendrier/calendrier.component';
+import { InventaireComponent } from './inventaire/inventaire.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { LivreComponent } from './livre/livre.component';
+import { InfosComponent } from './infos/infos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calendrier', component: CalendrierComponent },
+  { path: 'inventaire', component: InventaireComponent },
+  { path: 'reservations', component: ReservationsComponent },
+  { path: 'livre', component: LivreComponent },
+  { path: 'infos', component: InfosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendrierComponent } from './calendrier/calendrier.component';
+import { InventaireComponent } from './inventaire/inventaire.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { LivreComponent } from './livre/livre.component';
+import { InfosComponent } from './infos/infos.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendrierComponent,
+    InventaireComponent,
+    ReservationsComponent,
+    LivreComponent,
+    InfosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
