e2975c2a74709f525b0e9cc419319e830e85b74d
philippe <philippaudier@gmail.com>
2019-07-22 03:05:11 +0200

    prevent app from sleeping

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,8 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const http = require("http");
+setInterval(() => {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
