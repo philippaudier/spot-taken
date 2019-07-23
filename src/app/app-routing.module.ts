df512c63e2b34ef91bd82a226b411514e33fc18a
philippe <philippaudier@gmail.com>
2019-07-23 22:36:03 +0200

    inventaire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CalendrierComponent } from './calendrier/calendrier.component';
-import { InventaireComponent } from './inventaire/inventaire.component';
+import { TableBasic } from './inventaire/inventaire.component';
 import { ReservationsComponent } from './reservations/reservations.component';
 import { LivreComponent } from './livre/livre.component';
 import { InfosComponent } from './infos/infos.component';
 
 const routes: Routes = [
   { path: 'calendrier', component: CalendrierComponent },
-  { path: 'inventaire', component: InventaireComponent },
+  { path: 'inventaire', component: TableBasic },
   { path: 'reservations', component: ReservationsComponent },
   { path: 'livre', component: LivreComponent },
   { path: 'infos', component: InfosComponent }
