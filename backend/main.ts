809418031109db498245d87aa0e67b7ef3d35c1a
philippe <philippaudier@gmail.com>
2019-07-23 16:37:46 +0200

    con_string

--- backend/main.ts
+++ backend/main.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 import { Pool, Client } from "pg";
 
 require("source-map-support").install(); //to get stack traces pointing to ts files
-const connectionString = 'postgres://postgres:tiliChat1@server:5432/postgres';
+const connectionString = 'postgres://postgres:tiliChat1@server:server/postgres';
 const port = process.env.PORT || 8081;
 let databaseClient;
 
