73699f526af4ede75a5a19a4727071b608491213
philippe <philippaudier@gmail.com>
2019-07-23 21:14:14 +0200

    removed no sleep

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-const http = require("http");
-setInterval(() => {
-    http.get("http://<your app name>.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
