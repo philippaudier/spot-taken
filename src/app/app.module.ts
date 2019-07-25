dac2ad6c190ffb724c54a51ad7e1f67f026f1984
philippe <philippaudier@gmail.com>
2019-07-25 22:06:33 +0200

    no api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,7 +67,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatListModule,
     MatDividerModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyA4fcbnnuq3zkLijx31oa5-NhKUfQx2Cy0'
+      apiKey: ''
     }),
     MatCarouselModule,
     MatInputModule,
